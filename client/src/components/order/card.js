0332d211c8c9343c048b9be96026a3ceea8a174d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-12-02 15:54:38 +0100

    create card for orders

---
+++ client/src/components/order/card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Card() {
+    return (
+        <div>
+            <div>
+                <p>Buy Orders</p>
+                <table>
+                    <tr>
+                        <th>Security</th>
+                        <th>Qty</th>
+                        <th>Bid price</th>
+                    </tr>
+                    <tr>
+                        <td>data</td>
+                        <td>data</td>
+                        <td>data</td>
+                        <td>data</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    )
+}

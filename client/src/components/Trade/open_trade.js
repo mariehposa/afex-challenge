b472304f126663364c0928b56d02779b93f9994d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-12-04 16:57:02 +0100

    separate open trade into a component

--- client/src/components/Trade/open_trade.js
+++ client/src/components/Trade/open_trade.js
@@ -14,42 +14,8 @@ import {
   StyledP,
   SelectTradeCover
 } from "./styles";
-import New from './new_trade';
-import Open from './new_trade';
 
-export default function Trade() {
-  return (
-    <StyledOverall>
-      <StyledAllTrade>
-        <div>
-            <TradeNav to="/">New Trade</TradeNav>
-            <TradeNav to="/open">Open Trades</TradeNav>
-            <TradeNav to="/closed">Closed Trades</TradeNav>
-        </div>
-
-        <SelectCover>
-          <StyledSelect>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
-            <option value="30">30</option>
-            <option value="50">50</option>
-            <option value="100">100</option>
-          </StyledSelect>
-        </SelectCover>
-      </StyledAllTrade>
-
-      <>
-        <Route exact path="/" component={New} />
-        <Route path="/open" component={Open} />
-        <Route path="/closed" component={Closed} />
-      </>
-      {/* <BuyOrder /> */}
-    </StyledOverall>
-  );
-}
-
-export function Closed() {
+export function Open() {
     return(
         <StyleAllTrade>
             <StyledTradeTable>
@@ -58,11 +24,13 @@ export function Closed() {
                         <th>Transaction</th>
                         <th>Security</th>
                         <th>Units</th>
+                        <th>Matched Units</th>
+                        <th>Open Units</th>
                         <th>Order Price</th>
                         <th>Created</th>
-                        <th>Matched</th>
-                        <th>Type</th>
                         <th>Status</th>
+                        <th>Type</th>
+                        <th>Actions</th>
                     </StyledTradeRow>
                 </thead>
                 

c3f0d402acd892a6d92a46fc0c5d8621fed5806c
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-12-04 14:44:57 +0100

    add data for buy order

---
+++ client/src/data/buy_order.js
@@ -0,0 +1,27 @@
+export const fetc = [
+    {id: 1, cash_bal: 0.00, qty_owned: 0, current_price: 10000.00, buy_price: 10000, min_price: 9750, max_price: 10250, estimated_value: 10000}
+]
+
+export const ssbs = [
+    {id: 1, current_price: 17800.00, buy_price: 17800, min_price: 17355, max_price: 18245, estimated_value: 17800}
+]
+
+export const ssgm = [
+    {id: 1, current_price: 17500.00, buy_price: 17500, min_price: 17062.5, max_price: 17937.5, estimated_value: 17500}
+]
+
+export const smaz = [
+    {id: 1, current_price: 14800.00, buy_price: 14800, min_price: 14430, max_price: 15170, estimated_value: 14800}
+]
+
+export const cexp = [
+    {id: 1, current_price: 5000000.00, buy_price: 5000000, min_price: 5125000, max_price: 4875000, estimated_value: 5000000}
+]
+
+export const sprl = [
+    {id: 1, current_price: 19400.00, buy_price: 19400, min_price: 18915, max_price: 19885, estimated_value: 19400}
+]
+
+export const scoc = [
+    {id: 1, current_price: 96000.00, buy_price: 96000, min_price: 93600, max_price: 98400, estimated_value: 96000}
+]
